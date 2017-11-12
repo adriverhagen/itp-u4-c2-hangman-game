f18834f0c6719c4448f253258642a216bd9113e2
Adriana <adriana.verhagen@gmail.com>
2017-11-12 17:07:32 +0000

    updated

--- hangman/game.py
+++ hangman/game.py
@@ -1,24 +1,70 @@
+import random
 from .exceptions import *
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+LIST_OF_WORDS = [hello, hippopotamus, townhall, zumba, what]
 
 
 def _get_random_word(list_of_words):
-    pass
+    if len(list_of_words) != 0:
+        return random.choice(list_of_words)
+    raise InvalidListOfWordsException
 
 
 def _mask_word(word):
-    pass
+    if len(word) == None:
+        raise InvalidWordException()
+    return "*"*len(word)
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    if len(masked_word) or len(answer_word) == None:
+        raise InvalidWordException()
+        
+    if len(character)>1:
+        raise InvalidGuessedLetterException()
+    
+    if len(masked_word) != len(answer_word):
+        raise InvalidWordException()
+
+    answer = answer_word.lower()
+    if character.lower() not in answer:
+        return masked_word
+
+    new_word = ''
+    for answer_char, masked_char in zip(answer, masked_word):
+        if character.lower() == answer_char:
+            new_word += answer_char
+        else:
+            new_word += masked_char
+
+    return new_word
 
 
 def guess_letter(game, letter):
-    pass
+    letter = letter.lower()
 
+    if letter in game['previous_guesses']:
+        raise InvalidGuessedLetterException()
+
+    if _is_game_finished(game):
+        raise GameFinishedException()
+        
+    previous_masked = game['masked_word']
+    new_masked = _uncover_word(game['answer_word'], previous_masked, letter)
+    
+    if previous_masked == new_masked
+        game['remaining_misses'] -= 1
+    else:
+        game['masked_word'] = new_masked
+        
+    game['previous_guesses'].append(letter)
+    
+    if _is_game_won(game):
+        raise GameWonException()
+    
+    if _is_game_lost(game):
+        raise GameLostException()
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
     if list_of_words is None:
